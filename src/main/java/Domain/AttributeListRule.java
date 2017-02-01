594e0a8628429771ad0169230a21d93a67763004
paweltje <pawel.niewolik@student.hu.nl>
2017-02-01 22:03:46 +0100

    empty

--- src/main/java/Domain/AttributeListRule.java
+++
@@ -1,7 +0,0 @@
-package Domain;
-
-/**
- * Created by Polle on 1-2-2017.
- */
-public class AttributeListRule implements Rule{
-}
--- src/main/java/Domain/Rule.java
+++
@@ -1,9 +0,0 @@
-package Domain;
-
-/**
- * Created by Polle on 1-2-2017.
- */
-public interface Rule {
-    public String generateConstraint();
-    public Database getDataBase();
-}
