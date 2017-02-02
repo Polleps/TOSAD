b922a32f7f6802453f38c515b15d607be1d79100
paweltje <pawel.niewolik@student.hu.nl>
2017-02-02 22:45:27 +0100

    attribureCompareRuele update

--- src/main/java/Domain/AttributeCompareRule.java
+++ src/main/java/Domain/AttributeCompareRule.java
@@ -24,7 +24,7 @@ public class AttributeCompareRule implements Rule {
     public String generateConstraint() {
         String constraint = "";
         if(db instanceof SQLDatabase || db instanceof OracleDatabase){
-            constraint = "ALTER TABLE " + attr.getTable() + "ADD CONSTRAINT " + name + " CHECK (" + attr + " " + Controller.translateOperator(operator, "sql") + " " + val + ");";
+            constraint = "ALTER TABLE " + attr.getTable() + "ADD (CONSTRAINT " + name + " CHECK (" + attr + "" + Controller.translateOperator(operator, "sql") + "" + val + "))";
         }
         else{
             Controller.printToConsole("ERROR: AttributeCompareRule is not supported for this database!");
